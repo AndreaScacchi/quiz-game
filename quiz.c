39be009100494fc1a911d42a28b4d75f2522cb0c
AndreaScacchi <andreascacchi10@gmail.com>
2022-07-23 16:54:28 +0200

    main function implemented

--- quiz.c
+++ quiz.c
@@ -2,5 +2,6 @@
 #include <ctype.h>
 
 int main() {
+    char questions[][100] = {"1. What year did the C language debut?:", "2. Who is credited with creating C?:", "3. What is the predecessor of C?: "};
     
 }
